2ee4e56bccb47c43a7012f169a847bf59a43f1aa
astrale <autonabee@gmail.com>
2024-11-17 15:53:13 +0100

    temp simplify subscriber

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -19,7 +19,7 @@ import threading
 
 class mqtt_subscriber():
     """ Mqtt subscriber to receive a raw rotational speed"""
-    def __init__(self, on_message, synchro, topics, broker_addr='localhost'):
+    def __init__(self, on_message, synchro : threading.Lock, topics, broker_addr='localhost'):
         """
         Class constructor
 
@@ -42,15 +42,13 @@ class mqtt_subscriber():
         self.mqttBroker = broker_addr
         self.topics = topics
         # Lock thread synchronization
-        self.lock = synchro
-        self.lock.acquire()
-        self.debug=True
+        # self.lock = synchro
+        # self.lock.acquire()
 
 
     def subscribe_connect(self):
         """ client function launched in a thread
         """
-        if self.debug==True: print("Start mqtt subscriber")
         # Broker connection
         client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1,"Console") #preciser la version de paho a utiliser
         client.connect(self.mqttBroker) 
@@ -62,7 +60,6 @@ class mqtt_subscriber():
         # Wait for the end
         self.lock.acquire()
         client.loop_stop()
-        if self.debug==True: print("End mqtt subscriber")
    
     def run(self):
         """ Start the thread """
@@ -74,18 +71,18 @@ class mqtt_subscriber():
         self.lock.release()
         self.t1.join()
 
-if __name__ == "__main__":
-    """ Exemple of subscriber use"""
+# if __name__ == "__main__":
+#     """ Exemple of subscriber use"""
 
-    lock = threading.Lock()
+#     lock = threading.Lock()
     
-    def get_msg(self, client, userdata, message):
-        try:
-            rot_speed=float(str(message.payload.decode("utf-8"))) 
-        except Exception:
-            print('Error in mqtt message')
-            self.speed=0
-            self.rot_speed=0
+#     def get_msg(self, client, userdata, message):
+#         try:
+#             rot_speed=float(str(message.payload.decode("utf-8"))) 
+#         except Exception:
+#             print('Error in mqtt message')
+#             self.speed=0
+#             self.rot_speed=0
 
-    mqtt_sub=mqtt_subscriber(get_msg, lock, 'fit_and_fun/speed')
-    lock.acquire()
+#     mqtt_sub=mqtt_subscriber(get_msg, lock, 'fit_and_fun/speed')
+#     lock.acquire()
